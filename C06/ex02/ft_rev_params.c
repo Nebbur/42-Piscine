ffb69768c04226dbbaaf93c215846ebac74b40e1
Ruben Filipe Boia Pereira Soares <rboia-pe@c1r12s18.42porto.com>
2023-04-30 20:03:03 +0100

    100%

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rboia-pe <rboia-pe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 19:14:02 by rboia-pe          #+#    #+#             */
+/*   Updated: 2023/03/22 19:42:19 by rboia-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (argc > 0 && i < argc - 1)
+		i++;
+	while (i > 0)
+	{
+		ft_putstr(argv[i]);
+		write (1, "\n", 1);
+		i--;
+	}
+}
