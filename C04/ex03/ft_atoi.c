9dc7e8a389c55eb3763a4a519467583fdf627900
Ruben Filipe Boia Pereira Soares <rboia-pe@c1r12s18.42porto.com>
2023-04-30 20:02:39 +0100

    85%

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rboia-pe <rboia-pe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 09:24:15 by rboia-pe          #+#    #+#             */
+/*   Updated: 2023/03/20 09:24:40 by rboia-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	whitespaces(char *str, int *ptr_i)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == 43 || str[i] == 45)
+	{
+		if (str[i] == 45)
+			count *= -1;
+		i++;
+	}
+	*ptr_i = i;
+	return (count);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	result = 0;
+	sign = whitespaces(str, &i);
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
+
+/*int main()
+{
+	char *testamento = "   			+++--+-+165gf489765";
+	printf("%d\n", ft_atoi(testamento));
+}*/
---
+++ C04/ex04/ft_putnbr_base.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rboia-pe <rboia-pe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 09:58:55 by rboia-pe          #+#    #+#             */
+/*   Updated: 2023/03/21 08:57:49 by rboia-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	checkerror(char *str)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	len = ft_strlen(str);
+	i = 0;
+	if (str[0] == '\0' || len == 1)
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (str[i] <= 32 || str[i] == 127 || str[i] == 43 || str[i] == 45)
+			return (0);
+		j = i + 1;
+		while (j < len)
+		{
+			if (str[i] == str[j])
+				return (0);
+			j++;
+		}
+	i++;
+	}
+	return (1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	len;
+	int	error;
+	int	nb;
+
+	error = checkerror(base);
+	len = ft_strlen(base);
+	nb = nbr;
+	if (error == 1)
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb *= -1;
+		}
+		if (nb < len)
+		{
+			ft_putchar(base[nb]);
+		}
+		if (nb >= len)
+		{
+			ft_putnbr_base(nb / len, base);
+			ft_putnbr_base(nb % len, base);
+		}
+	}
+}
+
+/*int main()
+{
+<32 espaco para tras| 127 DEL | 43 + | 45 -
+	printf(" - Binario");
+	ft_putnbr_base(69, "01");
+	printf("\n - Decimal");
+	ft_putnbr_base(-696969, "0123456789");
+	printf("\n - Hexadecimal");
+	ft_putnbr_base(9898, "0123456789ABCDEF");
+	printf("\n - Octal");
+	ft_putnbr_base(27, "01234567");
+}*/
